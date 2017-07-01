98496416f30aa977556a6d65b230d8dc39e5431b
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2017-07-01 22:56:04 +0900

    整理

--- predict_vegetable_shipments/predict_vegetable.py
+++ predict_vegetable_shipments/predict_vegetable.py
@@ -22,12 +22,7 @@ def clean(x):
 weather_data_list = []
 for i in weather_df.columns.values:
     weather_data_list.append(weather_df[i].apply(clean).loc[1973:2012].values)
-# X = []
-# X.append(weather_data_list[2])  # 降雨量.
-# X.append(weather_data_list[6])  # 平均の気温.
-# X.append(weather_data_list[11])  # 平均湿度.
-# X.append(weather_data_list[13])  # 平均風速.
-# X.append(weather_data_list[18])  # 日照時間.
+
 X = pd.DataFrame({
     '降雨量': weather_data_list[2],
     '平均気温': weather_data_list[6],
@@ -43,24 +38,30 @@ chiba_df = chiba_df.apply(lambda x: x.replace('-', np.nan))
 # データ数が少ないため欠損値があるものは除外.
 chiba_df = chiba_df.dropna(axis=1)
 
-# naを0で埋める
-# chiba_df = chiba_df.fillna(value=0)
 vegetable_name_list = chiba_df.columns.levels[0].values
 
+# 収穫量用.
 amount_of_crops = {}
+# 作付面積用.
 planted_areas = {}
 for vegetable_name in vegetable_name_list:
     vegetable_df = chiba_df[vegetable_name]
-    if 'ha' not in vegetable_df['作付面積'].columns.values or 't' not in vegetable_df['収穫量'].columns.values:
+    if 'ha' not in vegetable_df['作付面積'].columns.values\
+            or 't' not in vegetable_df['収穫量'].columns.values:
         continue
     amount_of_crops[vegetable_name] = vegetable_df['収穫量'].t.values
     planted_areas[vegetable_name] = vegetable_df['作付面積'].ha.values
 
 
 X_cp = X.copy()
+# 平均二乗誤差用.
 scores = []
+# 予測値用.
 predictions = []
+# テストのインデックス用.
 test_index = []
+# y_test保持用.
+y_tests = []
 n_features = len(X.columns)
 space = [(1, n_features),  # max_features
          (1, 1e2),  # n_estimators
@@ -69,7 +70,12 @@ for vegetable_name in amount_of_crops.keys():
     df = X_cp
     df['作付面積'] = planted_areas[vegetable_name]
     df['収穫量'] = amount_of_crops[vegetable_name]
-    X_train, X_test, y_train, y_test = train_test_split(df.drop(['収穫量'], axis=1), df['収穫量'], test_size=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.drop(['収穫量'], axis=1),
+        df['収穫量'],
+        test_size=0.3
+    )
+    y_tests.append(y_test)
     test_index.append(X_test.index)
     scaler = StandardScaler()
     X_train = scaler.fit_transform(X_train)
@@ -85,6 +91,7 @@ for vegetable_name in amount_of_crops.keys():
 
         return -np.mean(cross_val_score(reg, X_train, y_train, cv=5, n_jobs=-1,
                                         scoring="neg_mean_absolute_error"))
+
     res_gp = gp_minimize(objective, space, n_calls=15, random_state=0)
     scores.append(res_gp.fun)
     reg.set_params(max_features=res_gp.x[0],
@@ -93,5 +100,5 @@ for vegetable_name in amount_of_crops.keys():
     reg.fit(X_train, y_train)
     predictions.append(reg.predict(X_test))
 
-for i, j, k, l in zip(amount_of_crops.keys(), test_index, scores, predictions):
-    print(i, j, k, l)
+for i, j, k, l, m in zip(amount_of_crops.keys(), test_index, scores, predictions, y_tests):
+    print(i, j, k, l, m)
