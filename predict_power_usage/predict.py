e2de921887249cdffa3e4000818cd17377481f16
Masaki Hikosaka <masaki_hikosaka@outlook.com>
2017-07-09 09:54:43 +0900

    電力消費量の予測モデルを追加

---
+++ predict_power_usage/predict.py
@@ -0,0 +1,161 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from skopt import gp_minimize
+from sklearn.model_selection import cross_val_score
+import numpy as np
+from skopt.plots import plot_convergence
+from sklearn.metrics import mean_squared_error
+import os
+import matplotlib.pyplot as plt
+import seaborn
+import math
+
+
+# 非線形回帰にR2は使用できないため平均二乗誤差のみ使用
+def get_weather_df(input_files, output_file, skiprows_dict):
+    if os.path.exists(output_file):
+        return pd.read_csv(output_file, encoding='sjis')
+    temperture_df = pd.read_csv(input_files['temperture'], encoding='sjis', skiprows=skiprows_dict['temperture'])
+    amount_of_precipitation_df = pd.read_csv(input_files['amount_of_precipitation'], encoding='sjis', skiprows=skiprows_dict['amount_of_precipitation'])
+    hours_of_sunlight_df = pd.read_csv(input_files['hours_of_sunlight'], encoding='sjis', skiprows=skiprows_dict['hours_of_sunlight'])
+    wind_speed_df = pd.read_csv(input_files['wind_speed'], encoding='sjis', skiprows=skiprows_dict['wind_speed'])
+    weather_df = pd.DataFrame(data=temperture_df.iloc[:, 0].values, columns=['年月日時'])
+    weather_df['気温'] = temperture_df.iloc[:, 1].values
+    weather_df['降水量'] = amount_of_precipitation_df.iloc[:, 1].values
+    weather_df['日照時間'] = hours_of_sunlight_df.iloc[:, 1].values
+    weather_df['風速'] = wind_speed_df.iloc[1:, 1].values
+    weather_df.to_csv(output_file, index=None, encoding='sjis')
+    return weather_df
+
+
+def get_demand_date_list(filename, header):
+    """データの準備."""
+    demand_df = pd.read_csv(filename, encoding='sjis', header=header)
+    date_list = demand_df['DATE'].apply(lambda x: x.split('/')).values
+    time_list = demand_df['TIME'].apply(lambda x: x.split(':')[0]).values
+    month_list = []
+    day_list = []
+    for year, month, day in date_list:
+        month_list.append(month)
+        day_list.append(day)
+    return (demand_df.iloc[:, 2], month_list, day_list, time_list)
+
+
+def set_params(params, reg):
+    """パラメータの設定."""
+    n_estimators, max_depth, max_features, min_samples_split, min_samples_leaf = params
+
+    reg.set_params(n_estimators=n_estimators,
+                   max_depth=max_depth,
+                   max_features=max_features,
+                   min_samples_split=min_samples_split,
+                   min_samples_leaf=min_samples_leaf)
+    return reg
+
+
+def cross_validation(reg, X_train, y_train):
+    return -np.mean(cross_val_score(reg, X_train, y_train, cv=10, n_jobs=-1,
+                                    scoring='neg_mean_squared_error'))
+
+
+def prepare_data(input_filenames, output_filename, objective_filename, skiprows_list=[4, 4, 4, 4], demand_header=1):
+    input_files = {
+        'temperture': input_filenames[0],
+        'amount_of_precipitation': input_filenames[1],
+        'hours_of_sunlight': input_filenames[2],
+        'wind_speed': input_filenames[3]
+    }
+    skiprows_dict = {
+        'temperture': skiprows_list[0],
+        'amount_of_precipitation': skiprows_list[1],
+        'hours_of_sunlight': skiprows_list[2],
+        'wind_speed': skiprows_list[3]
+    }
+    weather_df = get_weather_df(input_files, output_filename, skiprows_dict)
+    demand_list, month_list, day_list, time_list = get_demand_date_list(objective_filename, demand_header)
+    weather_df['月'] = np.array(month_list, dtype='int')
+    weather_df['日'] = np.array(day_list, dtype='int')
+    weather_df['時'] = np.array(time_list, dtype='int')
+    X_y = weather_df
+    X_y['電力需要'] = demand_list
+    # nanを平均で補完
+    X_y = X_y.fillna(X_y.mean())
+    X_y
+    return (X_y.drop(['年月日時', '電力需要'], axis=1).astype('float').values, X_y['電力需要'].astype('float').values.reshape(-1, 1))
+
+
+if __name__ == '__main__':
+    input_filenames = ['temperture_2008.csv',
+                       'amount_of_precipitation_2008.csv',
+                       'hours_of_sunlight_2008.csv',
+                       'wind_speed_2008.csv']
+    X_train, y_train = prepare_data(input_filenames, 'weather_2008.csv', 'demand_2008.csv')
+
+    input_filenames = ['temperture_2010.csv',
+                       'amount_of_precipitation_2010.csv',
+                       'hours_of_sunlight_2010.csv',
+                       'wind_speed_2010.csv']
+    temp_X, temp_y = prepare_data(input_filenames, 'weather_2010.csv', 'demand_2010.csv')
+    X_train = np.vstack((X_train, temp_X))
+    y_train = np.vstack((y_train, temp_y))
+
+    input_filenames = ['temperture_2011.csv',
+                       'amount_of_precipitation_2011.csv',
+                       'hours_of_sunlight_2011.csv',
+                       'wind_speed_2011.csv']
+    temp_X, temp_y = prepare_data(input_filenames, 'weather_2011.csv', 'demand_2011.csv')
+    X_train = np.vstack((X_train, temp_X))
+    y_train = np.vstack((y_train, temp_y))
+
+    input_filenames = ['temperture_2012.csv',
+                       'amount_of_precipitation_2012.csv',
+                       'hours_of_sunlight_2012.csv',
+                       'wind_speed_2012.csv']
+    temp_X, temp_y = prepare_data(input_filenames, 'weather_2012.csv', 'demand_2012.csv')
+    X_train = np.vstack((X_train, temp_X))
+    y_train = np.vstack((y_train, temp_y))
+
+    input_filenames = ['temperture_2014.csv',
+                       'amount_of_precipitation_2014.csv',
+                       'hours_of_sunlight_2014.csv',
+                       'wind_speed_2014.csv']
+    temp_X, temp_y = prepare_data(input_filenames, 'weather_2014.csv', 'demand_2014.csv')
+    X_train = np.vstack((X_train, temp_X))
+    y_train = np.vstack((y_train, temp_y))
+
+    input_filenames = ['temperture_2016.csv',
+                       'amount_of_precipitation_2016.csv',
+                       'hours_of_sunlight_2016.csv',
+                       'wind_speed_2016.csv']
+    X_test, y_test = prepare_data(input_filenames, 'weather_2016.csv', 'demand_2016.csv', demand_header=2)
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    reg = RandomForestRegressor(random_state=0)
+
+    def objective(params):
+        global reg
+        reg = set_params(params, reg)
+        return cross_validation(reg, X_train, y_train)
+    space = [(1, 1e5),  # n_estimators
+             (1, 1e5),  # max_depth
+             (1, X_train.shape[1]),  # max_features
+             (2, 1e5),  # min_samples_split
+             (1, 1e5)]  # min_samples_leaf
+    res_gp = gp_minimize(objective, space, n_calls=10, random_state=0, verbose=0, n_jobs=-1)
+    math.sqrt(res_gp.fun)
+    reg = set_params(res_gp.x, reg)
+    reg.fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    math.sqrt(mean_squared_error(y_pred, y_test))
+    plot_convergence(res_gp)
+    plt.figure(figsize=(20, 10))
+    plt.plot(range(0, X_test.shape[0]), y_test)
+    plt.plot(range(0, X_test.shape[0]), y_pred)
+    plt.show()
--- predict_vegetable_shipments/predict_vegetable.py
+++ predict_vegetable_shipments/predict_vegetable.py
@@ -6,31 +6,36 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score
 from skopt import gp_minimize
+import math
 
+AMOUNT_OF_CROP = '収穫量'
+PLANTED_AREA = '作付面積'
 weather_dfs = pd.read_html('http://www.data.jma.go.jp/obd/stats/etrn/view/annually_s.php?prec_no=45&block_no=47682&year=&month=&day=&view=', skiprows=3, index_col=0)
 
 chiba_df = pd.read_excel('chiba.xls', header=[4, 5, 6], skip_footer=3)
 
 weather_df = weather_dfs[0]
 
-
 def clean(x):
     if isinstance(x, str):
         return x.replace(']', '')
 
 
-weather_data_list = []
-for i in weather_df.columns.values:
-    weather_data_list.append(weather_df[i].apply(clean).loc[1973:2012].values)
+def get_weather_df(start_year, end_year):
+    weather_data_list = []
+    for i in weather_df.columns.values:
+        weather_data_list.append(weather_df[i].apply(clean).loc[start_year:end_year].values)
+    X = pd.DataFrame({
+        '降雨量': weather_data_list[2],
+        '平均気温': weather_data_list[6],
+        '平均湿度': weather_data_list[11],
+        '平均風速': weather_data_list[13],
+        '日照時間': weather_data_list[18]
+    }, index=range(start_year, end_year+1), dtype='float')
+    return X
 
-X = pd.DataFrame({
-    '降雨量': weather_data_list[2],
-    '平均気温': weather_data_list[6],
-    '平均湿度': weather_data_list[11],
-    '平均風速': weather_data_list[13],
-    '日照時間': weather_data_list[18]
-}, index=range(1973, 2013), dtype='float')
 
+X = get_weather_df(1973, 2012)
 # 「…」と「_」はnanに置き換え
 chiba_df = chiba_df.apply(lambda x: x.replace('…', np.nan))
 chiba_df = chiba_df.apply(lambda x: x.replace('-', np.nan))
@@ -46,11 +51,11 @@ amount_of_crops = {}
 planted_areas = {}
 for vegetable_name in vegetable_name_list:
     vegetable_df = chiba_df[vegetable_name]
-    if 'ha' not in vegetable_df['作付面積'].columns.values\
-            or 't' not in vegetable_df['収穫量'].columns.values:
+    if 'ha' not in vegetable_df[PLANTED_AREA].columns.values\
+            or 't' not in vegetable_df[AMOUNT_OF_CROP].columns.values:
         continue
-    amount_of_crops[vegetable_name] = vegetable_df['収穫量'].t.values
-    planted_areas[vegetable_name] = vegetable_df['作付面積'].ha.values
+    amount_of_crops[vegetable_name] = vegetable_df[AMOUNT_OF_CROP].t.values
+    planted_areas[vegetable_name] = vegetable_df[PLANTED_AREA].ha.values
 
 
 X_cp = X.copy()
@@ -68,11 +73,11 @@ space = [(1, n_features),  # max_features
          (1, 1e2)]  # min_samples_leaf
 for vegetable_name in amount_of_crops.keys():
     df = X_cp
-    df['作付面積'] = planted_areas[vegetable_name]
-    df['収穫量'] = amount_of_crops[vegetable_name]
+    df[PLANTED_AREA] = planted_areas[vegetable_name]
+    df[AMOUNT_OF_CROP] = amount_of_crops[vegetable_name]
     X_train, X_test, y_train, y_test = train_test_split(
-        df.drop(['収穫量'], axis=1),
-        df['収穫量'],
+        df.drop([AMOUNT_OF_CROP], axis=1),
+        df[AMOUNT_OF_CROP],
         test_size=0.3
     )
     y_tests.append(y_test)
@@ -90,7 +95,7 @@ for vegetable_name in amount_of_crops.keys():
                        min_samples_leaf=min_samples_leaf)
 
         return -np.mean(cross_val_score(reg, X_train, y_train, cv=5, n_jobs=-1,
-                                        scoring="neg_mean_absolute_error"))
+                                        scoring="neg_mean_squared_error"))
 
     res_gp = gp_minimize(objective, space, n_calls=15, random_state=0)
     scores.append(res_gp.fun)
@@ -102,3 +107,55 @@ for vegetable_name in amount_of_crops.keys():
 
 for i, j, k, l, m in zip(amount_of_crops.keys(), test_index, scores, predictions, y_tests):
     print(i, j, k, l, m)
+
+reg = RandomForestRegressor()
+soy_df = pd.read_excel('soy.xls', header=[4, 5])
+soy_df = soy_df[[PLANTED_AREA, AMOUNT_OF_CROP]]
+df = get_weather_df(1966, 2012)
+soy_df = soy_df.loc['昭.41(1966)':]
+df[PLANTED_AREA] = soy_df[PLANTED_AREA].astype('float').values
+df[AMOUNT_OF_CROP] = soy_df[AMOUNT_OF_CROP].astype('float').values
+X_train, X_test, y_train, y_test = train_test_split(df.drop(AMOUNT_OF_CROP, axis=1), df[AMOUNT_OF_CROP], test_size=0.2)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+space = [(1, len(df.drop(AMOUNT_OF_CROP, axis=1).columns)),  # max_features
+         (1, 1e3),  # n_estimators
+         (1, 1e3)]  # min_samples_leaf
+
+
+def objective(params):
+    max_features, n_estimators, min_samples_leaf = params
+
+    reg.set_params(max_features=max_features,
+                   n_estimators=n_estimators,
+                   min_samples_leaf=min_samples_leaf)
+
+    return -np.mean(cross_val_score(reg, X_train, y_train, cv=5, n_jobs=-1,
+                                    scoring="neg_mean_squared_error"))
+
+
+res_gp = gp_minimize(objective, space, n_calls=200, random_state=0)
+# memo: 290.268
+math.sqrt(res_gp.fun)
+
+X_train, X_test, y_train, y_test = train_test_split(df[PLANTED_AREA], df[AMOUNT_OF_CROP], test_size=0.2)
+space = [(1, 1e3),  # n_estimators
+         (1, 1e3)]  # min_samples_leaf
+
+
+reg = RandomForestRegressor()
+reg.fit(X_train.values[:, None], y_train)
+def objective(params):
+    n_estimators, min_samples_leaf = params
+
+    reg.set_params(n_estimators=n_estimators,
+                   min_samples_leaf=min_samples_leaf)
+
+    return -np.mean(cross_val_score(reg, X_train.values[:, None], y_train, cv=5, n_jobs=-1,
+                                    scoring="neg_mean_squared_error"))
+
+res_gp = gp_minimize(objective, space, n_calls=50, random_state=0)
+# memo 466.93
+math.sqrt(res_gp.fun)
